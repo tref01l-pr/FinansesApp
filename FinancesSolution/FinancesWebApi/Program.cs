3dcd487a08ca5a12b31d261fa64fbf6f95893401
Roman Protsai <protsayroman228@gmail.com>
2024-02-28 01:29:33 +0100

    update seed

--- FinancesSolution/FinancesWebApi/Program.cs
+++ FinancesSolution/FinancesWebApi/Program.cs
@@ -1,3 +1,6 @@
+using FinancesWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinancesWebApi;
 
 public class Program
@@ -7,11 +10,31 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        builder.Services.AddTransient<Seed>();
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        });
 
         var app = builder.Build();
+
+        if (args.Length == 1 && args[0].ToLower() == "seeddata")
+            SeedData(app);
+
+        void SeedData(IHost app)
+        {
+            var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+            using (var scope = scopedFactory.CreateScope())
+            {
+                var service = scope.ServiceProvider.GetService<Seed>();
+                service.SeedDataContext();
+            }
+        }
 
         if (app.Environment.IsDevelopment())
         {
--- FinancesSolution/FinancesWebApi/Seed.cs
+++ FinancesSolution/FinancesWebApi/Seed.cs
@@ -13,95 +13,101 @@ namespace FinancesWebApi.Data
             _context = context;
         }
 
-        public void SeedData()
+        public void SeedDataContext()
         {
-            var iconColors = new List<IconColor>
+            if (!_context.IconColors.Any())
             {
-                new IconColor { Color = "#4e8e2e" },
-                new IconColor { Color = "#67ae44" },
-                new IconColor { Color = "#80cf5c" },
-                new IconColor { Color = "#8dd66b" },
-                new IconColor { Color = "#a3dd88" },
-                new IconColor { Color = "#b7e3a4" },
-                
-                new IconColor { Color = "#52664a" },
-                new IconColor { Color = "#6a8260" },
-                new IconColor { Color = "#859d79" },
-                new IconColor { Color = "#9bb58e" },
-                new IconColor { Color = "#bad6ad" },
-                new IconColor { Color = "#d2f1c5" },
-            };
-            
-            var iconCategories = new List<IconCategory>
-            {
-                new IconCategory { Name = "Finances" },
-                new IconCategory { Name = "Transportation" },
-                new IconCategory { Name = "Shopping" },
-                new IconCategory { Name = "Food And Drink" },
-                new IconCategory { Name = "Home" },
-                new IconCategory { Name = "Health" },
-                new IconCategory { Name = "Beauty" },
-                new IconCategory { Name = "Entertainment" },
-                new IconCategory { Name = "Accounts" },
-                new IconCategory { Name = "Workout" },
-                new IconCategory { Name = "Relaxation" },
-                new IconCategory { Name = "Education" },
-                new IconCategory { Name = "Family/Children" },
-                new IconCategory { Name = "Farm" },
-                new IconCategory { Name = "Other" },
-            };
-            
+                var iconColors = new List<IconColor>
+                {
+                    new IconColor { Color = "#4e8e2e" },
+                    new IconColor { Color = "#67ae44" },
+                    new IconColor { Color = "#80cf5c" },
+                    new IconColor { Color = "#8dd66b" },
+                    new IconColor { Color = "#a3dd88" },
+                    new IconColor { Color = "#b7e3a4" },
 
-            var expenseIcons = new List<ExpenseIcon>
-            {
-                new ExpenseIcon { Name = "coins", IconCategory = iconCategories[0] },
-                new ExpenseIcon { Name = "car", IconCategory = iconCategories[1] },
-                new ExpenseIcon { Name = "bicycle", IconCategory = iconCategories[1] },
-                new ExpenseIcon { Name = "car-side", IconCategory = iconCategories[1] },
-                new ExpenseIcon { Name = "plane", IconCategory = iconCategories[1] },
-            };
+                    new IconColor { Color = "#52664a" },
+                    new IconColor { Color = "#6a8260" },
+                    new IconColor { Color = "#859d79" },
+                    new IconColor { Color = "#9bb58e" },
+                    new IconColor { Color = "#bad6ad" },
+                    new IconColor { Color = "#d2f1c5" },
+                };
 
-            var expenseCategories = new List<ExpenseCategory>
-            {
-                new ExpenseCategory { Name = "Health", IsDefault = true, ExpenseIcon = expenseIcons[0], IconColor = iconColors[0]},
-                new ExpenseCategory { Name = "Home", IsDefault = true, ExpenseIcon = expenseIcons[1], IconColor = iconColors[1]}
-            };
+                var iconCategories = new List<IconCategory>
+                {
+                    new IconCategory { Name = "Finances" },
+                    new IconCategory { Name = "Transportation" },
+                    new IconCategory { Name = "Shopping" },
+                    new IconCategory { Name = "Food And Drink" },
+                    new IconCategory { Name = "Home" },
+                    new IconCategory { Name = "Health" },
+                    new IconCategory { Name = "Beauty" },
+                    new IconCategory { Name = "Entertainment" },
+                    new IconCategory { Name = "Accounts" },
+                    new IconCategory { Name = "Workout" },
+                    new IconCategory { Name = "Relaxation" },
+                    new IconCategory { Name = "Education" },
+                    new IconCategory { Name = "Family/Children" },
+                    new IconCategory { Name = "Farm" },
+                    new IconCategory { Name = "Other" },
+                };
 
-            
-            
-            var incomeIcons = new List<IncomeIcon>
-            {
-                new IncomeIcon { Name = "coins", IconCategory = iconCategories[2] },
-                new IncomeIcon { Name = "car", IconCategory = iconCategories[4] },
-                new IncomeIcon { Name = "bicycle", IconCategory = iconCategories[3] },
-                new IncomeIcon { Name = "car-side", IconCategory = iconCategories[6] },
-                new IncomeIcon { Name = "plane", IconCategory = iconCategories[2] },
-            };
-            
-            var incomeCategories = new List<IncomeCategory>
-            {
-                new IncomeCategory { Name = "Health", IsDefault = true, IncomeIcon = incomeIcons[0], IconColor = iconColors[0]},
-                new IncomeCategory { Name = "Home", IsDefault = true, IncomeIcon = incomeIcons[1], IconColor = iconColors[1]}
-            };
-            
 
-            try
-            {
-                _context.IconColors.AddRange(iconColors);
-                _context.IconCategories.AddRange(iconCategories);
-                _context.ExpenseIcons.AddRange(expenseIcons);
-                _context.ExpenseCategories.AddRange(expenseCategories);
-                _context.IncomeIcons.AddRange(incomeIcons);
-                _context.IncomeCategories.AddRange(incomeCategories);
+                var expenseIcons = new List<ExpenseIcon>
+                {
+                    new ExpenseIcon { Name = "coins", IconCategory = iconCategories[0] },
+                    new ExpenseIcon { Name = "car", IconCategory = iconCategories[1] },
+                    new ExpenseIcon { Name = "bicycle", IconCategory = iconCategories[1] },
+                    new ExpenseIcon { Name = "car-side", IconCategory = iconCategories[1] },
+                    new ExpenseIcon { Name = "plane", IconCategory = iconCategories[1] },
+                };
 
-                _context.SaveChanges();
+                var expenseCategories = new List<ExpenseCategory>
+                {
+                    new ExpenseCategory
+                        { Name = "Health", IsDefault = true, ExpenseIcon = expenseIcons[0], IconColor = iconColors[0] },
+                    new ExpenseCategory
+                        { Name = "Home", IsDefault = true, ExpenseIcon = expenseIcons[1], IconColor = iconColors[1] }
+                };
+
+
+                var incomeIcons = new List<IncomeIcon>
+                {
+                    new IncomeIcon { Name = "coins", IconCategory = iconCategories[2] },
+                    new IncomeIcon { Name = "car", IconCategory = iconCategories[4] },
+                    new IncomeIcon { Name = "bicycle", IconCategory = iconCategories[3] },
+                    new IncomeIcon { Name = "car-side", IconCategory = iconCategories[6] },
+                    new IncomeIcon { Name = "plane", IconCategory = iconCategories[2] },
+                };
+
+                var incomeCategories = new List<IncomeCategory>
+                {
+                    new IncomeCategory
+                        { Name = "Health", IsDefault = true, IncomeIcon = incomeIcons[0], IconColor = iconColors[0] },
+                    new IncomeCategory
+                        { Name = "Home", IsDefault = true, IncomeIcon = incomeIcons[1], IconColor = iconColors[1] }
+                };
+
+
+                try
+                {
+                    _context.IconColors.AddRange(iconColors);
+                    _context.IconCategories.AddRange(iconCategories);
+                    _context.ExpenseIcons.AddRange(expenseIcons);
+                    _context.ExpenseCategories.AddRange(expenseCategories);
+                    _context.IncomeIcons.AddRange(incomeIcons);
+                    _context.IncomeCategories.AddRange(incomeCategories);
+
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while seeding the database: {ex.Message}");
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred while seeding the database: {ex.Message}");
-            }
-            
-            
+
+
             if (!_context.Users.Any())
             {
                 var users = new List<User>
